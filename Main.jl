c237433a65ede54478d700143689f42833c837d3
Tomasz <pasternak.t@gmail.com>
2013-11-21 00:02:33 +0100

    Big data, single thread.

--- Main.jl
+++ Main.jl
@@ -46,10 +46,50 @@ function afPoints(sets)
     return x, y
 end
 
-function main()
-    iafdb = union( map(x -> "iafdb/iaf" *string(x) * "_afw", 1:10), map(x -> "iafdb/iaf" *string(x) * "_afw", 1:10))
-    fantasia = ["fantasia/f1o06"]
+const fantasia = [
+	"fantasia/f1o01"
+	"fantasia/f1o02"
+	"fantasia/f1o03"
+	"fantasia/f1o04"
+	"fantasia/f1o05"
+	"fantasia/f1o06"
+	"fantasia/f1o07"
+	"fantasia/f1o08"
+	"fantasia/f1o09"
+	"fantasia/f1o10"
+	"fantasia/f1y01"
+	"fantasia/f1y02"
+	"fantasia/f1y03"
+	"fantasia/f1y04"
+	"fantasia/f1y05"
+	"fantasia/f1y06"
+	"fantasia/f1y07"
+	"fantasia/f1y08"
+	"fantasia/f1y09"
+	"fantasia/f1y10"
+	"fantasia/f2o01"
+	"fantasia/f2o02"
+	"fantasia/f2o03"
+	"fantasia/f2o04"
+	"fantasia/f2o05"
+	"fantasia/f2o06"
+	"fantasia/f2o07"
+	"fantasia/f2o08"
+	"fantasia/f2o09"
+	"fantasia/f2o10"
+	"fantasia/f2y01"
+	"fantasia/f2y02"
+	"fantasia/f2y03"
+	"fantasia/f2y04"
+	"fantasia/f2y05"
+	"fantasia/f2y06"
+	"fantasia/f2y07"
+	"fantasia/f2y08"
+	"fantasia/f2y09"
+	"fantasia/f2y10"]
+const iafdb = union( map(x -> "iafdb/iaf" *string(x) * "_afw", 1:8), map(x -> "iafdb/iaf" *string(x) * "_afw", 1:8))
 
+function main()
     redX, redY = afPoints(iafdb)
     greenX, greenY = afPoints(fantasia)
     plot(redX, redY, "r.", greenX, greenY, "g.")
